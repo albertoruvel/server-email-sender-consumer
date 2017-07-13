d967bd8cc33491fc7bb372f90987a5d230cfccca
Alberto <arubalcaba@24hourfit.com>
2017-07-12 17:41:44 -0700

    Working new templates and requests distribution

--- src/main/java/com/messaging/email/consumer/service/impl/EmailSenderServiceImpl.java
+++ src/main/java/com/messaging/email/consumer/service/impl/EmailSenderServiceImpl.java
@@ -1,8 +1,11 @@
 package com.messaging.email.consumer.service.impl;
 
 import com.amazon.sqs.javamessaging.message.SQSTextMessage;
-import com.dareu.web.dto.request.email.EmailRequest;
+import com.dareu.web.dto.jms.EmailRequest;
+import com.dareu.web.dto.jms.EmailType;
+import com.dareu.web.dto.jms.WelcomeEmailPayload;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.messaging.email.consumer.exception.EmailSenderException;
 import com.messaging.email.consumer.service.EmailSenderService;
 import org.apache.log4j.Logger;
@@ -26,6 +29,7 @@ import java.io.StringWriter;
 public class EmailSenderServiceImpl implements EmailSenderService {
 
     private static final String ERROR_EMAIL_TEMPLATE_PATH = "/templates/error-message-template.vm";
+    private static final String WELCOME_USER_TEMPLATE_PATH = "/templates/welcome-user-template.vm";
 
     @Autowired
     private JavaMailSender javaMailSender;
@@ -50,23 +54,14 @@ public class EmailSenderServiceImpl implements EmailSenderService {
     @Override
     public void sendEmail(SQSTextMessage textMessage)  throws EmailSenderException {
         try{
-            final String jsonMessage = textMessage.getText();
-            final EmailRequest emailRequest = gson.fromJson(jsonMessage, EmailRequest.class);
-            final MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-            Template template;
-            EmailRequest.EmailType emailType = EmailRequest.EmailType.fromString(emailRequest.getEmailType());
+            final String emailTypeValue = textMessage.getStringProperty("emailType");
+            if(emailTypeValue == null || emailTypeValue.isEmpty())
+                throw new EmailSenderException("No emailType was found on message headers");
+            EmailType emailType = EmailType.fromString(emailTypeValue);
             if(emailType == null)
                 throw new EmailSenderException("No email type was provided");
-            switch(emailType){
-                case ERROR:
-                    template = velocityEngine.getTemplate(ERROR_EMAIL_TEMPLATE_PATH);
-                    sendErrorEmail(emailRequest, helper, template);
-                    break;
-                case ANOTHER_USER_ACTION:
-                    //todo: add more actions here
-                    break;
-            }
+
+            dispatchEmailRequest(textMessage, emailType);
         }catch(JMSException ex){
             throw new EmailSenderException("Error reading message content: " + ex.getMessage());
         }catch(MessagingException ex){
@@ -76,6 +71,41 @@ public class EmailSenderServiceImpl implements EmailSenderService {
         }catch(Exception ex){
             throw new EmailSenderException("Unknown Error: " + ex.getMessage());
         }
+    }
+
+    private void dispatchEmailRequest(SQSTextMessage textMessage, EmailType emailType)throws JMSException, MessagingException, ResourceNotFoundException{
+        Template template;
+        String jsonText = textMessage.getText();
+        //try to parse message depending on email type
+        final MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        EmailRequest emailRequest;
+        switch(emailType){
+            case ERROR:
+                emailRequest = gson.fromJson(jsonText, new TypeToken<EmailRequest<String>>(){}.getType());
+                template = velocityEngine.getTemplate(ERROR_EMAIL_TEMPLATE_PATH);
+                sendErrorEmail(emailRequest, helper, template);
+                break;
+            case USER_REGISTRATION:
+                emailRequest = gson.fromJson(jsonText, new TypeToken<EmailRequest<WelcomeEmailPayload>>(){}.getType());
+                template = velocityEngine.getTemplate(WELCOME_USER_TEMPLATE_PATH);
+                sendWelcomeUserEmail(emailRequest, helper, template);
+                break;
+        }
+    }
+
+    private void sendWelcomeUserEmail(EmailRequest<WelcomeEmailPayload> emailRequest, MimeMessageHelper helper, Template template)throws MessagingException {
+        //get user id
+        final WelcomeEmailPayload welcomePayload = emailRequest.getBody();
+        VelocityContext context = new VelocityContext();
+        context.put("request", welcomePayload);
+        helper.setTo(welcomePayload.getEmail());
+        final StringWriter writer = new StringWriter();
+        template.merge(context, writer);
+        helper.setText(writer.toString(), true);
+        helper.setFrom(mailUsername);
+        helper.setSubject(String.format("%s error", emailRequest.getApplicationId()));
+        javaMailSender.send(helper.getMimeMessage());
     }
 
     private final void sendErrorEmail(EmailRequest request, final MimeMessageHelper helper, Template template)throws MessagingException{
